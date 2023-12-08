b702e4b2b6d2691049b2a26a27a829c7fb0959ea
Timothy J. Aveni <tja@berkeley.edu>
2023-12-07 22:13:39 -0800

    Chat completion editor

--- reagent-client/src/noggin-editor/text-completion/Editor.tsx
+++ reagent-client/src/noggin-editor/text-completion/Editor.tsx
@@ -1,6 +1,13 @@
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import { Editor as SlateEditor, Node, Transforms, createEditor } from 'slate';
-import { Editable, ReactEditor, Slate, withReact } from 'slate-react';
+import {
+  Editable,
+  ReactEditor,
+  Slate,
+  useSelected,
+  withReact,
+  useSlate,
+} from 'slate-react';
 import { withHistory } from 'slate-history';
 
 import { getYjsDoc, syncedStore } from '@syncedstore/core';
@@ -35,9 +42,11 @@ const websocketProvider = new WebsocketProvider(
   doc,
 );
 
-const initialValue = [{
-  children: [{ text: '' }],
-}];
+const initialValue: any[] = [
+  // {
+  //   children: [{ text: '' }],
+  // },
+];
 
 const Parameter = ({
   attributes,
@@ -58,15 +67,199 @@ const Parameter = ({
   );
 };
 
-const withParameters = (editor: any) => {
-  const { isInline, isVoid } = editor;
+const ChatTurn = ({
+  attributes,
+  children,
+  element,
+}: {
+  attributes: any;
+  children: any;
+  element: any;
+}) => {
+  const selected = useSelected();
+
+  const editor = useSlate() as unknown as ReactEditor; // i really think this will work...
+
+  const setSpeaker = (speaker: 'user' | 'assistant') => {
+    const path = ReactEditor.findPath(editor, element);
+    Transforms.setNodes(editor, { speaker } as unknown as any, { at: path }); // yikers
+    // force slate rerender on paragraphs
+  };
+
+  const setUser = useCallback(() => {
+    setSpeaker('user');
+  }, []);
+
+  const setAssistant = useCallback(() => {
+    setSpeaker('assistant');
+  }, []);
+
+  return (
+    <div {...attributes} className="chat-turn" contentEditable={false}>
+      <div
+        className={'chat-turn-inner' + (selected ? ' selected' : '')}
+      >
+        {element.speaker === 'user' ? (
+          <div
+            className="chat-turn-button chat-turn-button-user"
+            role="button"
+            onClick={setAssistant}
+          >
+            {/* « User */}
+            User
+          </div>
+        ) : (
+          <div
+            className="chat-turn-button chat-turn-button-assistant"
+            role="button"
+            onClick={setUser}
+          >
+            {/* Assistant » */}
+            Assistant
+          </div>
+        )}
+      </div>
+      {children}
+    </div>
+  );
+};
+
+const TextFragment = ({
+  attributes,
+  children,
+  element,
+}: {
+  attributes: any;
+  children: any;
+  element: any;
+}) => {
+  const editor = useSlate() as unknown as ReactEditor; // todo
+  const path = ReactEditor.findPath(editor, element);
+
+  const previousTopLevelElement = [path[0] - 1];
+
+  const previousTopLevelElementNode = Node.get(editor, previousTopLevelElement);
+
+  let className;
+
+  // @ts-ignore
+  if (previousTopLevelElementNode?.speaker === 'user') {
+    className = 'text-paragraph-user';
+    // @ts-ignore
+  } else if (previousTopLevelElementNode?.speaker === 'assistant') {
+    className = 'text-paragraph-assistant';
+  }
+
+  return (
+    <p className={'text-paragraph ' + className} {...attributes}>
+      {children}
+    </p>
+  );
+};
+
+const withChatCompletionsElements = (editor: any) => {
+  const { isInline, isVoid, normalizeNode } = editor;
 
   editor.isInline = (element: any) => {
-    return element.type === 'parameter' ? true : isInline(element);
+    if (element.type === 'parameter') {
+      return true;
+    }
+
+    if (element.type === 'chat-turn') {
+      return false;
+    }
+
+    return isInline(element);
   };
 
   editor.isVoid = (element: any) => {
-    return element.type === 'parameter' ? true : isVoid(element);
+    if (element.type === 'parameter') {
+      return true;
+    }
+
+    if (element.type === 'chat-turn') {
+      return true;
+    }
+
+    return isVoid(element);
+  };
+
+  editor.normalizeNode = ([node, path]: [any, any]) => {
+    // we're normalizing the root node
+    if (path.length === 0) {
+      const insertTopChatTurn = () => {
+        // this is a little weird as a thing to do if they backspace the top chat turn and it was an 'assistant' turn and it converts to a user one -- really what we want to do is forbid the backspace, but this will do
+        Transforms.insertNodes(
+          editor,
+          {
+            type: 'chat-turn',
+            speaker: 'user',
+            children: [{ text: '' }],
+          } as unknown as Node,
+          { at: [0] },
+        );
+      };
+
+      // if no children or we aren't starting with a chat turn, add one.
+      // UNLESS the top paragraph is empty and the next thing is a chat turn -- then we should just delete the paragraph. this is the common mode if you delete the top chat turn but still want the other stuff in there
+      if (editor.children.length === 0) {
+        insertTopChatTurn();
+      } else {
+        const topChild = editor.children[0];
+        if (
+          topChild.type === 'paragraph' &&
+          topChild.children[0].text === '' &&
+          editor.children[1]?.type === 'chat-turn'
+        ) {
+          Transforms.removeNodes(editor, { at: [0] });
+        } else if (topChild.type !== 'chat-turn') {
+          insertTopChatTurn();
+        }
+      }
+
+      // go through the top-level elements. if any chat turn isn't followed by a paragraph, add one
+      for (let i = 0; i < editor.children.length; i++) {
+        const child = editor.children[i];
+        // console.log(1, child, i, editor.children.length);
+
+        if (
+          child.type === 'chat-turn' &&
+          ((i < editor.children.length - 1 &&
+            editor.children[i + 1].type !== 'paragraph') ||
+            i === editor.children.length - 1)
+        ) {
+          Transforms.insertNodes(
+            editor,
+            {
+              type: 'paragraph',
+              children: [{ text: '' }],
+            } as unknown as Node,
+            { at: [i + 1] },
+          );
+        }
+      }
+
+      // merge paragraphs that are next to each other
+      for (let i = 0; i < editor.children.length; i++) {
+        const child = editor.children[i];
+        // console.log(2, child);
+
+        if (
+          child.type === 'paragraph' &&
+          i < editor.children.length - 1 &&
+          editor.children[i + 1].type === 'paragraph'
+        ) {
+          const text =
+            child.children[0].text +
+            '\n' +
+            editor.children[i + 1].children[0].text;
+          Transforms.mergeNodes(editor, { at: [i + 1] });
+          i--; // hmm feels like i need this
+        }
+      }
+    }
+
+    return normalizeNode([node, path]);
   };
 
   return editor;
@@ -76,7 +269,6 @@ const withSoftBreak = (editor: any) => {
   const { insertBreak } = editor;
 
   editor.insertBreak = () => {
-    console.log('hey break');
     // const { selection } = editor;
 
     // if (selection) {
@@ -92,8 +284,18 @@ const withSoftBreak = (editor: any) => {
   };
 
   editor.insertSoftBreak = () => {
-    console.log('hey soft break');
-    return editor.insertText('\n');
+    // todo: add a chat turn, different from the one we'e currently in (the last one before the selection)
+
+    const { selection } = editor;
+
+    Transforms.insertNodes(editor, {
+      type: 'chat-turn',
+      speaker: 'user',
+      children: [{ text: '' }],
+    } as unknown as Node); // todo hehe
+    Transforms.move(editor);
+
+    // return editor.insertText('\n');
   };
 
   return editor;
@@ -101,11 +303,11 @@ const withSoftBreak = (editor: any) => {
 
 // todo: we will probably do this imperatively someday instead of on every update
 const getAllParams = (editor: ReactEditor) => {
-  console.log(
-    [...Node.nodes(editor)].filter(([node, path]: [any, number[]]) => {
-      return node.type === 'parameter';
-    }),
-  );
+  // console.log(
+  //   [...Node.nodes(editor)].filter(([node, path]: [any, number[]]) => {
+  //     return node.type === 'parameter';
+  //   }),
+  // );
 };
 
 const Editor = () => {
@@ -114,7 +316,7 @@ const Editor = () => {
 
   const editor = useMemo(() => {
     const e = withSoftBreak(
-      withParameters(
+      withChatCompletionsElements(
         withReact(
           withHistory(withYjs(createEditor(), promptDocumentContainer.xml!)),
         ),
@@ -146,8 +348,10 @@ const Editor = () => {
     switch (props.element.type) {
       case 'parameter':
         return <Parameter {...props} />;
+      case 'chat-turn':
+        return <ChatTurn {...props} />;
       default:
-        return <p {...props.attributes}>{props.children}</p>;
+        return <TextFragment {...props} />;
     }
   }, []);
 
@@ -179,6 +383,8 @@ const Editor = () => {
                 children: [{ text: '' }],
               } as unknown as Node); // todo hehe
               Transforms.move(editor);
+            } else if (event.key === '@') {
+              // event.preventDefault();
             }
           }}
         />
