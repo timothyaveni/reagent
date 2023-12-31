e4e504dbae38e27d88e736cd637ba40520cf791c
Timothy J. Aveni <tja@berkeley.edu>
2023-12-31 09:56:26 -0500

    Factor out DocType
    we had this duped before .. whoops

--- reagent-remix-vite/app/routes/noggins.$identifier.edit/noggin-editor/store.client.tsx
+++ reagent-remix-vite/app/routes/noggins.$identifier.edit/noggin-editor/store.client.tsx
@@ -1,41 +1,11 @@
 import { syncedStore } from '@syncedstore/core';
 import { WebsocketProvider } from 'y-websocket';
 import * as Y from 'yjs';
+import { DocType } from './DocType';
 
 export type NogginEditorStore = ReturnType<
   typeof initializeStoreForNoggin
 >['store'];
-
-type DocumentBaseParameter = {
-  name: string;
-};
-
-export interface DocumentTextParameter extends DocumentBaseParameter {
-  type: 'text';
-  maxLength: number;
-  defaultValue: string;
-}
-
-export interface DocumentImageParameter extends DocumentBaseParameter {
-  type: 'image';
-  openAI_detail: 'low' | 'high' | 'auto'; // openai-centric for now. also maybe in the future we do our own scaling in the shim?
-}
-
-export type DocumentParameter = DocumentTextParameter | DocumentImageParameter;
-type _DPTypeCheck = DocumentParameter['type'];
-
-type DocType = {
-  modelInputs: Record<string, Y.XmlText | any>;
-  documentParameters: Record<string, Y.Map<DocumentParameter>>;
-  // using Object.keys on documentParameters doesn't trigger a rerender on the index component, so we also keep a list of IDs so that the `push` gets noticed by the rerender logic...
-  documentParameterIdsByDocument: Record<string, string[]>;
-  nogginOptions: {
-    chosenOutputFormatKey: string;
-  };
-  syncState: {
-    synced?: boolean;
-  };
-};
 
 let globalWebsocketProvider: WebsocketProvider | null = null;
 
