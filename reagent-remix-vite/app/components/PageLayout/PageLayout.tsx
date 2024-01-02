dfded27eb64799cf4abaed4769c2dc28da993a5b
Timothy J. Aveni <tja@berkeley.edu>
2024-01-01 23:37:14 -0500

    Proper footer

--- reagent-remix-vite/app/components/PageLayout/PageLayout.tsx
+++ reagent-remix-vite/app/components/PageLayout/PageLayout.tsx
@@ -1,5 +1,6 @@
 import TopBar from './TopBar';
 
+import { Box, Stack, Link as StyledLink, Typography } from '@mui/material';
 import { useMatches } from '@remix-run/react';
 import './PageLayout.css';
 
@@ -18,12 +19,35 @@ export const PageLayout = ({
       <>
         <TopBar />
         <main className={wide ? 'wide' : ''}>{children}</main>
-        <footer
-          style={{
-            marginTop: 400,
-          }}
-        >
-          by tim
+        <footer>
+          <Box
+            sx={{
+              mt: 10,
+              mb: 4,
+            }}
+            textAlign={'center'}
+          >
+            <Stack>
+              <Typography variant="body2" color="textSecondary">
+                reagent was built by{' '}
+                <StyledLink href="https://timothyaveni.com/">
+                  Timothy J. Aveni
+                </StyledLink>{' '}
+                in the{' '}
+                <StyledLink href="https://bid.berkeley.edu/">
+                  Berkeley Institute of Design
+                </StyledLink>{' '}
+                lab at UC Berkeley.
+              </Typography>
+              <Typography variant="body2" color="textSecondary">
+                reagent's source code is available under the AGPLv3 license on{' '}
+                <StyledLink href="https://github.com/timothyaveni/reagent">
+                  GitHub
+                </StyledLink>
+                .
+              </Typography>
+            </Stack>
+          </Box>
         </footer>
       </>
     );
