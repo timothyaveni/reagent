06f9753472d686a781eb9adfd5c964f0d46d6ca7
Timothy J. Aveni <tja@berkeley.edu>
2024-01-06 19:36:38 -0800

    Get minio script working on dev and add other buckets

--- minio/init-buckets.sh
+++ minio/init-buckets.sh
@@ -2,8 +2,15 @@
 
 mc alias set myminio http://minio:9000 $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
 
+
 mc mb -p myminio/noggin-run-outputs
 mc anonymous set-json /minio-cfg/noggin-run-outputs-read-policy.json myminio/noggin-run-outputs
+
+mc mb -p myminio/noggin-run-inputs
+mc anonymous set-json /minio-cfg/noggin-run-inputs-read-policy.json myminio/noggin-run-inputs
+
+mc mb -p myminio/noggin-files
+mc anonymous set-json /minio-cfg/noggin-files-read-policy.json myminio/noggin-files
 
 
 mc admin user add myminio $OBJECT_STORAGE_ACCESS_KEY $OBJECT_STORAGE_SECRET_KEY
