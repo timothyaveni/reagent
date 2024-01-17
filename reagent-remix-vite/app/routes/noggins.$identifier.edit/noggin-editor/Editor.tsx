f3220775e5954fa086055811878710179b9e73ac
Timothy J. Aveni <tja@berkeley.edu>
2024-01-17 13:41:42 -0800

    Responsive 1-col editor page

--- reagent-remix-vite/app/routes/noggins.$identifier.edit/noggin-editor/Editor.tsx
+++ reagent-remix-vite/app/routes/noggins.$identifier.edit/noggin-editor/Editor.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 
-import { Box, Paper, Stack, Typography } from '@mui/material';
+import {
+  Box,
+  Paper,
+  Stack,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material';
 import { EditorSchema } from 'reagent-noggin-shared/types/editorSchema';
 import T from '~/i18n/T';
 import InputsColumn from './InputsColumn';
@@ -14,75 +21,107 @@ export interface EditorProps {
   editorSchema: EditorSchema;
 }
 
+const ModelInputsBox = ({ editorSchema }: EditorProps) => {
+  return (
+    <Box>
+      <Typography variant="h2" mb={2}>
+        Model inputs
+      </Typography>
+      <Paper elevation={2} sx={{ padding: 2 }}>
+        <InputsColumn
+          inputs={editorSchema.modelInputComponents.map((inputKey) => ({
+            inputKey,
+            input: editorSchema.allEditorComponents[inputKey],
+          }))}
+          column="primary"
+        />
+      </Paper>
+    </Box>
+  );
+};
+
+const ModelOutputsBox = ({ editorSchema }: EditorProps) => {
+  return (
+    <Box>
+      <Typography variant="h2" mb={2}>
+        Model output
+      </Typography>
+      <Paper elevation={2} sx={{ padding: 3 }}>
+        <ModelOutputEditor
+          outputFormats={editorSchema.outputFormats}
+          editorComponents={editorSchema.allEditorComponents} // TODO this maybe goes in context or we filter it in the prop-drill
+        />
+      </Paper>
+    </Box>
+  );
+};
+
+const ModelParametersBox = ({ editorSchema }: EditorProps) => {
+  return (
+    <Box>
+      <Typography variant="h2" gutterBottom>
+        Model parameters
+      </Typography>
+      <Typography
+        variant="body2"
+        component="p"
+        color="textSecondary"
+        // gutterBottom
+        mb={2}
+      >
+        <T flagged>
+          Tweak the model's behavior here. The default values are reasonable, so
+          you dont <em>need</em> to poke around in this section, but you may
+          find that your needs are better suited by changing these parameters.
+        </T>
+      </Typography>
+      <Paper elevation={2} sx={{ padding: 2 }}>
+        <InputsColumn
+          inputs={editorSchema.modelParameterComponents.map((inputKey) => ({
+            inputKey,
+            input: editorSchema.allEditorComponents[inputKey],
+          }))}
+          column="secondary"
+        />
+      </Paper>
+    </Box>
+  );
+};
+
 const Editor: React.FC<EditorProps> = (props) => {
   const { editorSchema } = props;
 
-  return (
-    <Stack direction="row" spacing={3} alignItems="top">
-      <Stack spacing={6} sx={{ flex: 2 }}>
-        <Box>
-          <Typography variant="h2" mb={2}>
-            Model inputs
-          </Typography>
-          <Paper elevation={2} sx={{ padding: 2 }}>
-            <InputsColumn
-              inputs={editorSchema.modelInputComponents.map((inputKey) => ({
-                inputKey,
-                input: editorSchema.allEditorComponents[inputKey],
-              }))}
-              column="primary"
-            />
-          </Paper>
-        </Box>
+  const theme = useTheme();
+  const isNarrow = useMediaQuery(theme.breakpoints.down('sm'));
 
-        <Box>
-          <Typography variant="h2" mb={2}>
-            Model output
-          </Typography>
-          <Paper elevation={2} sx={{ padding: 3 }}>
-            <ModelOutputEditor
-              outputFormats={editorSchema.outputFormats}
-              editorComponents={editorSchema.allEditorComponents} // TODO this maybe goes in context or we filter it in the prop-drill
-            />
-          </Paper>
-        </Box>
+  if (!isNarrow) {
+    return (
+      <Stack direction="row" spacing={3} alignItems="top">
+        <Stack spacing={6} sx={{ flex: 2 }}>
+          <ModelInputsBox {...props} />
+          <ModelOutputsBox {...props} />
+        </Stack>
+
+        <Stack spacing={6} sx={{ flex: 1 }}>
+          <AllVariableOptionControls
+            documentIds={editorSchema.modelInputComponents}
+          />
+          <ModelParametersBox {...props} />
+        </Stack>
       </Stack>
-
-      <Stack spacing={6} sx={{ flex: 1 }}>
+    );
+  } else {
+    return (
+      <Stack spacing={5}>
+        <ModelInputsBox {...props} />
         <AllVariableOptionControls
           documentIds={editorSchema.modelInputComponents}
         />
-        <Box>
-          <Typography variant="h2" gutterBottom>
-            Model parameters
-          </Typography>
-          <Typography
-            variant="body2"
-            component="p"
-            color="textSecondary"
-            // gutterBottom
-            mb={2}
-          >
-            <T flagged>
-              Tweak the model's behavior here. The default values are
-              reasonable, so you dont <em>need</em> to poke around in this
-              section, but you may find that your needs are better suited by
-              changing these parameters.
-            </T>
-          </Typography>
-          <Paper elevation={2} sx={{ padding: 2 }}>
-            <InputsColumn
-              inputs={editorSchema.modelParameterComponents.map((inputKey) => ({
-                inputKey,
-                input: editorSchema.allEditorComponents[inputKey],
-              }))}
-              column="secondary"
-            />
-          </Paper>
-        </Box>
+        <ModelOutputsBox {...props} />
+        <ModelParametersBox {...props} />
       </Stack>
-    </Stack>
-  );
+    );
+  }
 };
 
 export default Editor;
