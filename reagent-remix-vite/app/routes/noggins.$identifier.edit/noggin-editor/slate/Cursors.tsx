ba19ed8974009172383994aa7124e352a9135b55
Timothy J. Aveni <tja@berkeley.edu>
2024-01-17 13:27:59 -0800

    Make cursors rerender when they're supposed to

--- reagent-remix-vite/app/routes/noggins.$identifier.edit/noggin-editor/slate/Cursors.tsx
+++ reagent-remix-vite/app/routes/noggins.$identifier.edit/noggin-editor/slate/Cursors.tsx
@@ -3,9 +3,10 @@ import {
   // CursorOverlayData,
   useRemoteCursorOverlayPositions,
 } from '@slate-yjs/react';
-import { useRef } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 import { CaretPosition } from 'node_modules/@slate-yjs/react/dist/utils/getOverlayPosition';
+import { useSlateStatic } from 'slate-react';
 import './Cursors.css';
 
 type CursorsProps = {
@@ -22,6 +23,26 @@ export function Cursors({ children }: CursorsProps) {
   const [cursors] = useRemoteCursorOverlayPositions<CursorData>({
     containerRef,
   });
+  const editor = useSlateStatic();
+
+  // it turns out Cursors won't rerender if someone else types in the middle of the doc, because
+  // this gets dispatched as a 'local' event (long story). the official doc example is actually
+  // rerendering the parent editor component on every change. this feels slightly more responsible?
+  // does it make a difference? idk
+  const [, updateState] = useState({});
+  const forceUpdate = useCallback(() => updateState({}), []);
+  useEffect(() => {
+    const editorOnChange = editor.onChange;
+
+    editor.onChange = () => {
+      editorOnChange();
+      forceUpdate();
+    };
+
+    return () => {
+      editor.onChange = editorOnChange;
+    };
+  }, [editor]);
 
   return (
     <div className="cursors" ref={containerRef}>
