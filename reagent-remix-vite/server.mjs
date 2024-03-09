42f2ebc1e10c0e2ab165b2dd100e5fe06bacb1cf
Timothy J. Aveni <tja@berkeley.edu>
2024-03-08 23:41:11 -0800

    Add cookie expiration

--- reagent-remix-vite/server.mjs
+++ reagent-remix-vite/server.mjs
@@ -87,6 +87,7 @@ app.use(
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // 30 days
     // secure: process.env.NODE_ENV === 'production', // TODO: probably need trust proxy
   }),
 );
